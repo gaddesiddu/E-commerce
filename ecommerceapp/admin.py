83a9f88ab408e96339f47a7a5855cef75b72ca23
gaddesiddu <siddugadde118@gmail.com>
2024-01-05 13:17:35 +0530

    first commit

---
+++ ecommerceapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from ecommerceapp.models import Contact,Product,Orders,OrderUpdate
+admin.site.register(Contact)
+admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+
+
+# Register your models here.
